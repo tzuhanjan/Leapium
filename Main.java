c1fc51efe239234303a1036b86ba5e6ecc3bf968
Xiaohan Zhang <hellocharlien@hotmail.com>
2013-07-13 03:09:24 -0700

    basic Main framework

--- Main.java
+++ Main.java
@@ -1,4 +1,33 @@
-public class Main {
+
+import java.io.IOException;
+import java.lang.Math;
+import com.leapmotion.leap.*;
+import com.leapmotion.leap.Gesture.State;
+
+
+public class Main extends Listener {
+
+    public void onInit(Controller controller) {
+    }
+
+    public void onConnect(Controller controller) {
+        controller.enableGesture(Gesture.Type.TYPE_SWIPE);
+        controller.enableGesture(Gesture.Type.TYPE_KEY_TAP);
+
+    }
+
+    public void onDisconnect(Controller controller) {
+        //Note: not dispatched when running in a debugger.
+        System.out.println("Disconnected");
+    }
+
+    public void onExit(Controller controller) {
+        System.out.println("Exited");
+    }
+
+    public void onFrame(Controller controller) {
+        
+    }
 
     public static void main(String[] args) {
         System.out.println("LOLOLOL");
